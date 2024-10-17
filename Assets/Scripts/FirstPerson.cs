57ee5363ece4d4d39b150fc57692baf9da7eb055
AlexandraIonescuMiroiu <lyannisart@gmail.com>
2024-10-17 20:50:15 +0200

    ahh

--- Assets/Scripts/FirstPerson.cs
+++ Assets/Scripts/FirstPerson.cs
@@ -24,10 +24,17 @@ public class FirstPerson : MonoBehaviour
     {
       float h = Input.GetAxisRaw("Horizontal"); //h=0, h=-1, h=1
       float v = Input.GetAxisRaw("Vertical");  //v=0, v
-      Vector3 movimiento = new Vector3(h, 0, v),normalized;
+
+      ///Vector3 movimiento = new Vector3(h, 0, v).normalized;
+
+      Vector2 input = new Vector2 (h, v).normalized;
+      float anguloRotacion = Mathf.Atan2 (input.x, input.y) * Mathf.Rad2Deg;
+
+      transform.eulerAngles = new Vector3 (0,anguloRotacion, 0);
 
 
-        controller.Move(movimiento * velocidadMovimiento * Time.deltaTime);
+      //controller.Move(movimiento * velocidadMovimiento * Time.deltaTime);
+      
 
 
 
